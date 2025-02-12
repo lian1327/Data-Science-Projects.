aec5d69ac9eaa07e174dd1d0c0cc532bbc7a6d85
Leonard <leonardmesiera@outlook.com>
2025-02-12 09:55:41 +0800

    "tuples"

---
+++ excercise2.py
@@ -0,0 +1,10 @@
+# Create a tuple and print it.
+
+programming_tuples = ("Java", "C", "C++", "JavaScript","Python","Go","React", "Java")
+# Loop through tuple items by using a for loop.
+print("Languages I have used before")
+for i in programming_tuples:
+    print(i,"\n")
+
+# Use the count() method on a tuple.
+print(f"There are {programming_tuples.count("Java")} instance or copies ")
